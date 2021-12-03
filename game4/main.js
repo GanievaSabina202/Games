ca5a9d0d5fe81b4214f429719a1d7a3901c97d66
GanievaSabina202 <61120111+GanievaSabina202@users.noreply.github.com>
2021-12-04 01:38:43 +0400

    Game created version 2

--- game4/main.js
+++ game4/main.js
@@ -1,26 +1,39 @@
 questionWrapper = {
     q1: {
-        text: "What1?",
-        answer: ["a", "b", "c"],
+        text: "Question1 ?",
+        answer: ["a", "b", "c", "d"],
         trueAnswer: "a"
     },
     q2: {
-        text: "What2?",
-        answer: ["a2", "b2", "c2"],
-        trueAnswer: "c"
+        text: "Question2 ?",
+        answer: ["a", "b", "c", "d"],
+        trueAnswer: "b"
     },
     q3: {
-        text: "What3?",
-        answer: ["a3", "b3", "c3"],
+        text: "Question3 ?",
+        answer: ["a", "b", "c", "d"],
+        trueAnswer: "c"
+    },
+    q4: {
+        text: "Question4 ?",
+        answer: ["a", "b", "c", "d"],
+        trueAnswer: "c"
+    },
+    q5: {
+        text: "Question5 ?",
+        answer: ["a", "b", "c", "d"],
         trueAnswer: "c"
     }
 }
 class questionGame {
     headerQuestion = document.querySelector("#header-question")
     questionVariantWrapper = document.querySelector(".questionVariantWrapper")
+    winWrapper = document.querySelector("#winWrapper")
+    LoseWrapper = document.querySelector("#LoseWrapper")
     questions = [];
     questionIndex = 0;
     win = 0;
+    lose = 0;
     uservariant = null
     constructor(obj) {
         this.questions = Object.values(obj)
@@ -30,17 +43,35 @@ class questionGame {
             alert("A B C");
             return
         }
-    }
-    showQuestionFunc() {
-        this.headerQuestion.innerHTML = this.questions[this.questionIndex].text
-        this.questionVariantWrapper.innerHTML =
-            this.questions[this.questionIndex].answer.map((i) => {
-                return `<span >${i}</span>`
-            }).join("")
+        this.uservariant = item
+        this.nextQuestionFunc()
     }
 
-    nextQuestionFunc(){
-        
+    showQuestionFunc() {
+        this.headerQuestion.innerHTML = this.questions[this.questionIndex].text
+        this.questionVariantWrapper.innerHTML = this.questions[this.questionIndex].answer.map((i) => {
+            return `
+            <div class="col-6">
+                <span>${i}</span>
+            </div>`
+        }).join("")
+    }
+
+    nextQuestionFunc() {
+        let TrueAnswer = this.questions[this.questionIndex].trueAnswer
+        if (!this.questions) {
+            alert("Bosdur Bura")
+        }
+        if (TrueAnswer == this.uservariant) {
+            this.winWrapper.innerHTML =
+                `<span id="winWrapper">Xal: ${this.win +=10}</span>`
+        } else {
+            this.LoseWrapper.innerHTML =
+                `<span id="LoseWrapper">Səhv cavablar:  ${this.lose +=1}</span>`
+        }
+        this.questionIndex++
+        this.showQuestionFunc()
+        this.aa()
     }
 
 
