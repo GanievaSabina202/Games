e067b63d28e1fa0f6256b2a116fd1a6dae3b9f8b
GanievaSabina202 <61120111+GanievaSabina202@users.noreply.github.com>
2021-12-14 02:06:02 +0400

    game7

---
+++ game7/main.js
@@ -0,0 +1,32 @@
+var gamer = {
+    "a": {
+        name: "aa",
+        health: 20,
+        attack: 1
+    },
+    "b": {
+        name: "bb",
+        health: 16,
+        attack: 2
+
+    },
+    "c": {
+        name: "cc",
+        health: 14,
+        attack: 5
+
+    },
+    "d": {
+        name: "dd",
+        health: 17,
+        attack: 7
+    }
+}
+
+var aaa = Object.entries(gamer)
+console.log(aaa[2][1].health);
+
+$(".container").on("click",".btnGamer", function(){
+    var name = $(this).attr("id","he");
+
+})
